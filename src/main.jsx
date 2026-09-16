29fd2c3f6c7d286ef0cef7ac898e1fc47e313b8b
Nyamesem Gracious <eaduboahene39@gmail.com>
2026-09-16 17:07:50 +0000

    Refresh mobile UI to match approved ProofPay screens (#9)
    * feat: add exact ProofPay mobile screen system
    
    * style: match approved ProofPay mobile mockups
    
    * feat: make approved mobile experience the staging entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.jsx";
-import "./styles.css";
+import { MobileExact } from "./MobileExact.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MobileExact />
   </React.StrictMode>,
 );
-
-if ("serviceWorker" in navigator && import.meta.env.PROD) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js", { updateViaCache: "none" }).catch(() => {});
-  });
-}
